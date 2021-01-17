9ef80bdb16710f4eb4e2dd7f6f6b9a81b73d4550
bangsyir <bangsyir@gmail.com>
2021-01-18 00:11:15 +0800

    telegram bot api example

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var request = require('request')
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Telegram Bot' });
+});
+
+router.post('/send-notification', (req, res) => {
+  request.post({
+    url: `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, 
+    form: {
+      chat_id: '@belihubdotcom',
+      parse_mode: 'HTML',
+      text: `
+<b>From:</b> ${req.body.fullname}
+------------------------------------------
+<b>Email:</b> ${req.body.email}
+<b>Message:</b>
+${req.body.message}
+      `
+  }}, function(err, res, body) {
+    console.log(body)
+  })
+  res.redirect('back')
+})
+
+router.get('/telegram', (req, res) => {
+  res.send('telegram')
+})
+
+module.exports = router;
